d99a7d0bf1aa5a479887f378a9f9fda624ed8b08
Max Natanaelsson <max.natanaelsson@outlook.com>
2026-03-13 09:25:14 +0100

    Header added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="_header-container">
+        <div className="_header">
+          <i class="fa-solid fa-bus"></i>
+        </div>
+        <div className="_header-content">
+          <h1 className="_header-title">GoWest</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
