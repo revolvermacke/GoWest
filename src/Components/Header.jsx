be65f7f93777c3b4e11bcaa72d01eff804547a69
revolvermacke <max.natanaelsson@outlook.com>
2026-03-17 15:47:11 +0100

    added css changes in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,18 +4,17 @@ import BurgerMenu from "./BurgerMenu";
 
 const Header = () => {
   return (
-    <>
-      <div className="_header-container">
+    <div className="_header-wrapper">
+      <div className="_header-container _container">
         <div className="_header">
           <Logotype />
-          {/* <i class="fa-solid fa-bus"></i> */}
         </div>
         <div className="_header-content">
           <h1 className="_header-title">GoWest</h1>
         </div>
         <BurgerMenu />
       </div>
-    </>
+    </div>
   );
 };
 
