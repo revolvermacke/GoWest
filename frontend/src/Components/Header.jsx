c03b35214ddbbb42fc60a6361ccdaced4ee8d447
revolvermacke <max.natanaelsson@outlook.com>
2026-05-07 14:25:41 +0200

    Refactor authentication claims and enhance header component with user info display

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,18 +1,40 @@
 import "./Header.css";
 import Logotype from "./Logotype";
 import BurgerMenu from "./BurgerMenu";
+import { jwtDecode } from "jwt-decode";
 
 const Header = () => {
+  const token = localStorage.getItem("token");
+
+  let user = null;
+
+  if (token) {
+    user = jwtDecode(token);
+  }
+
   return (
     <div className="_header-wrapper">
       <div className="_header-container _container">
-        <div className="_header">
-          <Logotype />
+        <div className="_header-left">
+          <div className="_header">
+            <Logotype />
+          </div>
+
+          <div className="_header-content">
+            <h1 className="_header-title">GoWest</h1>
+          </div>
         </div>
-        <div className="_header-content">
-          <h1 className="_header-title">GoWest</h1>
+
+        <div className="_header-right">
+          {user && (
+            <span className="_userClaim">
+              <i className="fa-regular fa-user"></i>
+              {user.email}
+            </span>
+          )}
+
+          <BurgerMenu />
         </div>
-        <BurgerMenu />
       </div>
     </div>
   );
